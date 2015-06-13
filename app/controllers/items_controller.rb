52061afbed5a5170ee307ce06081c33ee367fba4
Atsushi Toyokura <atsushi.toyokura@gmail.com>
2015-06-13 18:08:13 +0900

    formからデータを受け取る処理を作りました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,38 @@ class ItemsController < ApplicationController
 
   def show
    @item = Item.find(params[:id]) # idでデータベースを検索する
- end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # フォームから投げられたオブジェクトを格納する
+    # item_paramsの定義は下部に記載
+    # params[:item] とかだったかな…
+    @item = Item.new(item_params)
+
+    # これを保存する
+    @item.save
+
+    # show.html.erbに飛ばす
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  def item_params
+    # params.require(:key).permit(:filter)
+    params.require(:item).permit(
+      :name,
+      :description,
+      :price,
+      :seller_id,
+      :email,
+      :image_url
+       )
+  end
 end
 
 
